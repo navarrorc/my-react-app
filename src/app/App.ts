dd8ef8980b56626210479b157ba0c80abd2d45e6
Roberto Navarro <navarrorc@users.noreply.github.com>
2015-10-18 01:02:56 -0500

    Minor fix

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,7 +2,7 @@
 require('../sass/styles.scss');
 
 import {MyComponent} from './components/MyComponent';
-import {MyService} from '../services/service';
+import {MyService} from './services/service';
 
 var myComponent = new MyComponent(null)
 myComponent.showComponent();
